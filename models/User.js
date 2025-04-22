b56b13d9e2a0f38f5e6eefa1e6f3da222168a641
__|c13n__ <98982131+lc13n@users.noreply.github.com>
2025-04-22 13:20:47 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,9 @@
-// models/User.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    // Thêm các trường khác nếu cần
 });
 
 const User = mongoose.model('User ', userSchema);
