acfc79a8931961ece81b985264d5cd78d36fd6ca
__|c13n__ <98982131+lc13n@users.noreply.github.com>
2025-04-22 13:17:15 +0700

    Update CartController.js

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,5 +1,4 @@
-// controllers/CartController.js
-const Cart = require('../models/Cart'); // Giả sử bạn có model Cart
+const Cart = require('../models/Cart'); 
 
 exports.viewCart = async (req, res) => {
     const cart = await Cart.findOne({ user: req.user.id }).populate('products.product');
@@ -8,23 +7,19 @@ exports.viewCart = async (req, res) => {
 
 exports.addToCart = async (req, res) => {
     const { productId } = req.body;
-    // Logic để thêm sản phẩm vào giỏ hàng
     res.send('Product added to cart');
 };
 
 exports.updateCart = async (req, res) => {
     const { productId, quantity } = req.body;
-    // Logic để cập nhật số lượng sản phẩm trong giỏ hàng
     res.send('Cart updated');
 };
 
 exports.removeFromCart = async (req, res) => {
     const { productId } = req.body;
-    // Logic để xóa sản phẩm khỏi giỏ hàng
     res.send('Product removed from cart');
 };
 
 exports.clearCart = async (req, res) => {
-    // Logic để xóa toàn bộ giỏ hàng
     res.send('Cart cleared');
 };
